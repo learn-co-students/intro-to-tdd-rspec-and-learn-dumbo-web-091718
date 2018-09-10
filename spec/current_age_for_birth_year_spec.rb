651b37fc4d8037349e87f19488f0c7a393e09b0b
Amir Safoev <amirsafoev0@gmail.com>
2018-09-10 19:25:20 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,3 +1,4 @@
+
 require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
@@ -6,6 +7,4 @@ describe "current_age_for_birth_year method" do
 
     expect(age_of_person).to eq(19)
   end
-end
-def = current_age_for_birth_year
 end
